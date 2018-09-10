a4d52c151f905357b87f28dc50cf3836f9d0bc50
Cassie <ccrowe1@unca.edu>
2018-09-10 15:47:35 -0400

    Update ZooManifestMaker.py

--- ZooManifestMaker.py
+++ ZooManifestMaker.py
@@ -3,7 +3,7 @@
 #
 # ZooManifestMaker.py
 #
-# Makes a manifest for Zooniverse image upload
+# Makes a CSV manifest for Zooniverse image upload
 #
 # To run: python ZooManifestMaker.py, put in the location for each image set
 
@@ -12,45 +12,6 @@ import numpy as np
 import glob
 import csv
 from astropy.io import fits
-
-hold1=[]
-hold2=[]
-CIV1=[]
-CIVpmf1=[]
-MGII1=[]
-MGIIpmf1=[]
-CIV2=[]
-CIVpmf2=[]
-MGII2=[]
-MGIIpmf2=[]
-MGII1=[]
-MGIIpmf1=[]
-
-SetLocal=str(raw_input("Please input the full location of the first (CIV) image set: "))  ## ask for the first image set location
-SetLocal2e=str(raw_input("Please input the full location of the second (MGII) image set: "))  ## ask for the second image set location
-
-hold1=glob.glob(SetLocal) ## makes list of CIV image file names
-hold2=glob.glob(SetLocal2) ## makes list of MGII image file names
-
-for i in range(len(hold1)):
-    s=hold1[i]
-    CIVpmf1.append(s[45:])
-    CIV1.append(s[51:66])
-for i in range(len(hold2)):
-    t=hold2[i]
-    MGIIpmf2.append(t[46:])
-for i in range(len(CIV1)):
-    a=CIV1[i]
-    for i in range(len(MGII2)):
-        if MGII2[i]==a:
-            MGII1.append(MGII2[i])
-    for i in range(len(CIVpmf1)):
-        b=MGIIpmf2[i]
-        if b[6:21]==a:
-            MGIIpmf1.append(MGIIpmf2[i])
-
-
-            
 
 hdu=fits.open("/Users/sumrsch/Summer_Research/DR14Q_v4_4.fits")
 tbdata = hdu[1].data
@@ -61,15 +22,49 @@ Zvi=tbdata["Z_VI"]
 Zpipe=tbdata["Z_PIPE"]
 PLATE=tbdata["PLATE"]
 FIBER=tbdata["FIBERID"]
-BI=tbdata["BI_CIV"]
 
 B=[]
 z=[]
 
-for i in range(len(CIV1)):
-    p=CIVpmf1[i][6:10]
-    f=CIVpmf1[i][11:15]
-    m=CIVpmf1[i][16:21]
+hold1=[]
+hold2=[]
+CIVpmf1=[]
+CIVfile1=[]
+MGIIpmf1=[]
+MGIIfile1=[]
+CIVfile2=[]
+CIVpmf2=[]
+MGIIpmf2=[]
+MGIIfile2=[]
+
+SetLocal=str(raw_input("Please input the full location of the first (CIV) image set: "))  ## ask for the first image set location
+SetLocal2e=str(raw_input("Please input the full location of the second (MGII) image set: "))  ## ask for the second image set location
+
+hold1=glob.glob(SetLocal) ## makes list of CIV image file names
+hold2=glob.glob(SetLocal2) ## makes list of MGII image file names
+
+for i in range(len(hold1)):      ## for-loop no.1: appends the PMF from CIV image filepath to CIVpmf1 array and filename to CIVfile1 
+    s=hold1[i]
+    CIVfile1.append(s[45:])
+    CIVpmf1.append(s[51:66])
+for i in range(len(hold2)):      ## for-loop no.2: appends filename to MGIIfile2 array
+    t=hold2[i]
+    MGIIfile2.append(t[46:])
+for i in range(len(CIVpmf1)):    ## for-loop no.3: loops through to line up CIV and MGII at the same indexes in their arrays
+    a=CIV1[i]
+    for i in range(len(MGII2)):             
+        if MGIIpmf2[i]==a:
+            MGIIpmf1.append(MGIIpmf2[i])
+    for i in range(len(CIVfile1)):
+        b=MGIIfile2[i]
+        if b[6:21]==a:
+            MGIIfile1.append(MGIIfile2[i])
+
+
+for i in range(len(CIV1)):       ## for-loop no.4: assigns plate, fiber, mjd, redshift and balnicity to their own arrays
+    p=CIVfile1[i][6:10]
+    f=CIVfile1[i][11:15]
+    m=CIVfile1[i][16:21]
     zp=tbdata[tbdata["PLATE"]==int(p)]
     zf=zp[zp["FIBERID"]==int(f)]
     z.append(zf["Z_VI"])
@@ -83,18 +78,18 @@ count=1
 #k=0
 #print(len(hold1))
 print("CIV1")
-for i in range(len(CIV1)):
+for i in range(len(CIV1)):      ## writes the final csv manifest
     #print(i)
     man=[]
     count=int(count)
     man=np.append(man,str(count))
     #print(man)
     #print(CIV1[k])
-    man=np.append(man,CIVpmf1[i])
-    man=np.append(man,MGIIpmf1[i])
-    man=np.append(man,CIVpmf1[i][6:10])
-    man=np.append(man,CIVpmf1[i][11:15])
-    man=np.append(man,CIVpmf1[i][16:21])
+    man=np.append(man,CIVfile1[i])
+    man=np.append(man,MGIIfile1[i])
+    man=np.append(man,CIVfile1[i][6:10])
+    man=np.append(man,CIVfile1[i][11:15])
+    man=np.append(man,CIVfile1[i][16:21])
     man=np.append(man,B[i])
     man=np.append(man,z[i])
     print(i,man)
